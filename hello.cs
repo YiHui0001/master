e92397cdc6a27ac797fdda14c774d3c0c2ecb30a
YiHui <YiHui.hsiao@seec.com.tw>
2024-04-23 08:36:55 +0800

    add a hello world(cs)

---
+++ hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsRadioCheckFrameTimerPrgBar
+{
+    class hello
+    {
+        public static void Main() {
+            Console.WriteLine("Hello World");
+        }                     
+    }
+}
